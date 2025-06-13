0f1bd2ab12732888ac114c980d17e96e01715aae
flopau76 <flopau76210@gmail.com>
2025-06-13 15:47:27 +0200

    simplify script

--- script.sh
+++ script.sh
@@ -3,12 +3,13 @@
 #SBATCH --job-name=test_graphs
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
 #SBATCH --partition=seqbio
 #SBATCH --qos=seqbio
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
+#SBATCH --array=1-8
 
 module purge
 module load SeqKit
@@ -20,7 +21,7 @@ export PATH=$PATH:"$PWD/rust_dbg/target/release"
 path_fasta="./data/scerevisiae8.fa.gz"
 path_split="./data/scerevisiae8_splits"
 path_graphs="./data/scerevisiae8_graphs"
-k=31
+path_encoding="./data/scerevisiae8_encoding"
 
 #____________________________________________________
 # Split fasta by individuals
@@ -29,36 +30,55 @@ k=31
 # srun seqkit split $path_fasta -i --id-regexp "^([\w]+)#" -O $path_split
 
 #____________________________________________________
-# Get the name of the newly created files
-#____________________________________________________
-
-# Initialize an empty array to store file names
-file_names=()
-
-# Iterate over the items in the folder
-for item in "$path_split"/*; do
-    # Check if the item is a file
-    if [ -f "$item" ]; then
-        # Append the file name to the array
-        file_names+=("$(basename "$item")")
-    fi
-done
-
-# Sort the file names
-IFS=$'\n' sorted_file_names=($(sort <<<"${file_names[*]}"))
-unset IFS
-
-#____________________________________________________
 # Create graphs containing different number of individuals
 #____________________________________________________
+# k=31
 
-mkdir -p $path_graphs
-for i in $(seq 1 ${#sorted_file_names[@]}); do
-    temp_file_list=()
-    for j in $(seq 0 $((i - 1))); do
-        temp_file_list+=("$path_split/${sorted_file_names[j]}")
-    done
-    srun ggcat build -k $k ${temp_file_list[@]} -o $path_graphs/n${i}_k${k}.fna --min-multiplicity 1 -j 4
-    srun rust_dbg -k $k -g $path_graphs/n${i}_k${k}.bin build -i $path_graphs/n${i}_k${k}.fna
-done
-rm $path_graphs/*.stats.log
+# fasta_names=("$path_split"/*.fa.gz)
+# IFS=$'\n' sorted_fasta_names=($(sort <<<"${fasta_names[*]}"))
+# unset IFS
+
+# mkdir -p $path_graphs
+# for i in $(seq 1 ${#sorted_fasta_names[@]}); do
+#     srun ggcat build -k $k "${sorted_fasta_names[@]:0:i}" -o $path_graphs/n${i}_k${k}.fna --min-multiplicity 1 -j $SLURM_CPUS_PER_TASK
+#     srun rust_dbg -k $k -g $path_graphs/n${i}_k${k}.bin build -i $path_graphs/n${i}_k${k}.fna
+# done
+# rm $path_graphs/*.stats.log
+
+#____________________________________________________
+# Get stats about graphs
+#____________________________________________________
+
+# k=31
+
+# for n in {1..8}; do
+#     srun rust_dbg -k $k -g $path_graphs/n${i}_k${k}.bin stats-g
+# done
+
+#____________________________________________________
+# Encode paths
+#____________________________________________________
+n=8
+k=23
+
+mkdir -p $path_encoding
+
+input_files=("$path_split"/*.fa.gz)
+path_input="${input_files[$((SLURM_ARRAY_TASK_ID - 1))]}"
+fasta_id=$(basename "$path_input" | sed -E 's/.*\.part_([^.]+)\.fa\.gz/\1/')
+path_output="$path_encoding/n${n}_k${k}_${fasta_id}.bin"
+
+echo "Encoding fasta: $file_name" >&2
+srun rust_dbg -k $k -g $path_graphs/n${n}_k${k}.bin encode -i "$path_input" -o "$path_output"
+
+#____________________________________________________
+# Get stats about paths
+#____________________________________________________
+# n=8
+# k=23
+
+# for file in $path_encoding/*; do
+#     if [[ -f $file ]]; then
+#         srun rust_dbg -k $k -g $path_graphs/n${n}_k${k}.bin stats-p -i $file
+#     fi
+# done
